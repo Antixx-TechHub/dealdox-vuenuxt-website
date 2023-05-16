5937bb03d19ed5c6d00875c402aa25c89383870b
Srohanrs <prashant.bhosale@antixxtechhub.com>
2023-05-16 15:16:36 +0530

    form validationdone

--- components/ContactUs/PageHeader.vue
+++ components/ContactUs/PageHeader.vue
@@ -25,55 +25,55 @@
                                             <div class="col-lg-6 col-md-6 col-sm-6">
                                                 <div class="form-group">
                                                     <p>Your good name!</p>
-                                                    <input type="text" name="name" class="form-control" id="name"
+                                                    <input type="text" name="name" required class="form-control" id="name" 
                                                         placeholder="Eg: Sarah">
                                                 </div>
                                             </div>
                                             <div class="col-lg-6 col-md-6 col-sm-6">
                                                 <div class="form-group">
                                                     <p>Last Name</p>
-                                                    <input type="text" name="name" class="form-control" id="name"
+                                                    <input type="text" name="name" required class="form-control" id="name"
                                                         placeholder="Eg: Taylor">
                                                 </div>
                                             </div>
                                             <div class="col-lg-6 col-md-6 col-sm-6">
                                                 <div class="form-group">
                                                     <p>Your email</p>
-                                                    <input type="email" name="email" class="form-control" id="email"
+                                                    <input type="email" name="email" required class="form-control" id="email"
                                                         placeholder="hello@sarah.com">
                                                 </div>
                                             </div>
                                             <div class="col-lg-6 col-md-6 col-sm-6">
                                                 <div class="form-group">
                                                     <p>Mobile</p>
-                                                    <input type="text" name="phone_number" class="form-control"
+                                                    <input type="text" name="phone_number" required class="form-control"
                                                         id="phone_number" placeholder="Enter your phone number">
                                                 </div>
                                             </div>
                                             <div class="col-lg-6 col-md-6 col-sm-6">
                                                 <div class="form-group">
                                                     <p>Company</p>
-                                                    <input type="text" name="name" class="form-control" id="name"
+                                                    <input type="text" name="name" class="form-control" required id="name"
                                                         placeholder="Eg: Lorem Ipsum">
                                                 </div>
                                             </div>
                                             <div class="col-lg-6 col-md-6 col-sm-6">
                                                 <div class="form-group">
                                                     <p>Website</p>
-                                                    <input type="text" name="name" class="form-control" id="name"
+                                                    <input type="text" name="name" required class="form-control" id="name"
                                                         placeholder="Eg: www.yourdomain.com">
                                                 </div>
                                             </div>
                                             <div class="col-lg-12 col-md-12 col-sm-12">
                                                 <div class="form-group">
                                                     <p>Please write your message</p>
-                                                    <textarea name="message" id="message" class="form-control" cols="30"
+                                                    <textarea name="message" id="message" required class="form-control" cols="30"
                                                         rows="6"
                                                         placeholder="This just a dummy message! please dont mind"></textarea>
                                                 </div>
                                             </div>
                                             <div class="col-lg-12 col-md-12 col-sm-12">
-                                                <button type="submit" class="default-btn"><i class='bx bx-paper-plane'></i>
+                                                <button type="submit" required class="default-btn"><i class='bx bx-paper-plane'></i>
                                                     Submit</button>
                                             </div>
                                         </div>
