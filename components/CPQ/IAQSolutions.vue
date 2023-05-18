3a031421799f85d024afe212d130458438c6a2f2
AntixxTechHub <info@antixxtechhub.com>
2023-05-18 19:14:47 +0530

    CPQ css fixed

--- components/CPQ/IAQSolutions.vue
+++ components/CPQ/IAQSolutions.vue
@@ -1,110 +1,22 @@
 <template>
     <div class="stats-area-pages bg-white ptb-100">
         <div class="container">
-            <div class="section-title-pages">
-                <h1>Transform Your Sales Process with Our <span class="sub-title-pages">Integrated Automated Quoting
-                    </span>Solutions</h1>
-            </div>
-            <div class="stats-map text-center">
-                <img src="../../assets/images/cpq/transform_sales.png" alt="cpq">
-            </div>
-
-            <div class="screenshots-swiper-slides">
-                <!-- <carousel :autoplay="5000" :settings='settings' :breakpoints='breakpoints' :wrap-around="true">
-                    <slide v-for="slide in carouselItems" :key="slide.id">
-                        <div class="swiper-slide">
-                            <img :src="slide.image" alt="screenshots">
-                        </div>
-                    </slide>
-
-                   <template #addons>
-                        <navigation>
-                            <template #next>
-                                <i class='ri-arrow-right-s-line'></i>
-                            </template>
-                            <template #prev>
-                                <i class='ri-arrow-left-s-line'></i>
-                            </template>
-                        </navigation>
-                    </template>
-                </carousel> -->
+            <div class="section-title-pages-full">
+                <div class="new-app-main-banner-wrap-content-full">
+                    <h2>Transform Your Sales Process with Our <span class="sub-title">Integrated Automated
+                            Quoting</span> Solutions</h2>
+                </div>
+                <div class="stats-map text-center">
+                    <img src="../../assets/images/banner/banner-home.png" alt="cpq">
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-// import { Carousel, Slide, } from 'vue3-carousel';
 
-// import 'vue3-carousel/dist/carousel.css';
 export default {
     name: 'Stats',
-    // components: {
-    //     Carousel,
-    //     Slide,
-    //     // Navigation,
-    // },
-    // data: () => ({
-    //     settings: {
-    //         itemsToShow: 1,
-    //         snapAlign: 'center',
-    //     },
-    //     carouselItems: [
-    //         {
-    //             id: 1,
-    //             image: require('../../assets/images/screenshots/screen1.png'),
-    //         },
-    //         // {
-    //         //     id: 2,
-    //         //     image: require('../../assets/images/screenshots/screen2.jpg'),
-    //         // },
-    //         // {
-    //         //     id: 3,
-    //         //     image: require('../../assets/images/screenshots/screen3.jpg'),
-    //         // },
-    //         // {
-    //         //     id: 4,
-    //         //     image: require('../../assets/images/screenshots/screen4.jpg'),
-    //         // },
-    //         // {
-    //         //     id: 5,
-    //         //     image: require('../../assets/images/screenshots/screen5.jpg'),
-    //         // },
-    //         // {
-    //         //     id: 6,
-    //         //     image: require('../../assets/images/screenshots/screen6.jpg'),
-    //         // },
-    //         // {
-    //         //     id: 7,
-    //         //     image: require('../../assets/images/screenshots/screen7.jpg'),
-    //         // },
-    //         // {
-    //         //     id: 8,
-    //         //     image: require('../../assets/images/screenshots/screen8.jpg'),
-    //         // },
-    //     ],
-    //     breakpoints: {
-    //         0: {
-    //             itemsToShow: 1,
-    //             snapAlign: 'left',
-    //         },
-    //         576: {
-    //             itemsToShow: 3,
-    //             snapAlign: 'center',
-    //         },
-    //         768: {
-    //             itemsToShow: 3,
-    //             snapAlign: 'center',
-    //         },
-    //         992: {
-    //             itemsToShow: 3,
-    //             snapAlign: 'center',
-    //         },
-    //         1200: {
-    //             itemsToShow: 2.1,
-    //             snapAlign: 'center',
-    //         },
-    //     },
-    // }),
 }
 </script>
--- components/CPQ/Stats.vue
+++ components/CPQ/Stats.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="stats-area bg-FAFAFA ptb-100">
+    <div class="key-features-area bg-#FAFAFA pt-100 pb-75">
         <div class="container">
-            <div class="section-title">
+            <div class="section-title title-with-bg-text">
                 <h2>What is CPQ for Services?</h2>
                 <p>CPQ is a sales tool that quickly generates accurate quotes by automating the process based on pre-set
                     rules. It seamlessly integrates with CRM and ERP technologies to ensure data accuracy. CPQ software
@@ -9,69 +9,50 @@
                     Services CPQ software fills the gap in service pricing and quotes, streamlining pre-sales practices and
                     reducing manual errors. Generate professional quotes in just a few minutes with CPQ for services.</p>
             </div>
-            <br>
-            <br>
-            <div class="section-title">
-            <h3>CPQ Software: The ultimate tool for multi team collaboration</h3>
-            <p>Master the art of remote collaboration </p>
+
+            <div class="section-title title-with-bg-text">
+                <h3>CPQ Software: The ultimate tool for multi team collaboration</h3>
+                <p>Master the art of remote collaboration </p>
             </div>
 
-
-            <!-- <div class="stats-map text-center">
-                <img src="../../assets/images/cpq/cpq-services.svg" alt="cpq">
-            </div> -->
-            <div class="row justify-content-center ">
+            <div class="row justify-content-center">
                 <div class="col-xl-4 col-lg-6 col-sm-6 col-md-6">
-                    <div class="single-features-items ">
-                        <h6> Sales </h6>
-                        <p>DealHub automates time-consuming manual processes and provides all the tools needed to close deals, so sales reps
-                            can focus on selling.
-                        </p>
+                    <div class="key-features-card">
+                        <div class="icon">
+                            <i class="ri-eye-line"></i>
+                        </div>
+                        <h3>Sales</h3>
+                        <p>Just log in with your mail account from play store and using whatever you want for your able
+                            business purpose.</p>
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-6 col-sm-6 col-md-6">
-                    <div class="single-features-items">
-                        <h6>Operations </h6>
-                        <p>Our no-code environment  enables operations teams to continuously adapt to changing business needs, like pricing and
-                            product updates, in real time.
-                        </p>
+                    <div class="key-features-card bg-color-two">
+                        <div class="icon bg2">
+                            <i class="ri-stack-line"></i>
+                        </div>
+                        <h3>Operations</h3>
+                        <p>Just log in with your mail account from play store and using whatever you want for your able
+                            business purpose.</p>
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-6 col-sm-6 col-md-6">
-                    <div class="single-features-items">
-                        
-                        <h6>Finance </h6>
-                        <p>Increase revenue growth with improved discount governance, quote version control, and advanced
-                            approval workflows.
-                        </p>
+                    <div class="key-features-card">
+                        <div class="icon">
+                            <i class="ri-leaf-line"></i>
+                        </div>
+                        <h3>Finance</h3>
+                        <p>Just log in with your mail account from play store and using whatever you want for your able
+                            business purpose.</p>
                     </div>
                 </div>
             </div>
-            <!-- <div class="container">
-                <div class="row">
-                    <div class="col-sm">
-                        <h5>Sales</h5>
-                        <p> DealHub automates time-consuming manual processes and provides all the tools needed to close
-                            deals, so sales reps can focus on selling. </p>
-                    </div>
-                    <div class="col-sm">
-                        <h5>Operations</h5>
-                        <p>Our no-code environment enables operations teams to continuously adapt to changing business
-                            needs, like pricing and product updates, in real time. </p>
-                    </div>
-                    <div class="col-sm">
-                        <h5>Finance</h5>
-                        <p> Increase revenue growth with improved discount governance, quote version control, and advanced
-                            approval workflows. </p>
-                    </div>
-                </div>
-            </div> -->
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Stats'
+    name: 'IncludedBestFeatures'
 }
 </script>
