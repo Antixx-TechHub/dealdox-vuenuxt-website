f44347ee2573a455a9e8e735e6f9db2f54ccb7ee
AntixxTechHub <info@antixxtechhub.com>
2023-06-13 23:24:03 +0530

    cpq update

--- components/CPQ/FreeTrial.vue
+++ components/CPQ/FreeTrial.vue
@@ -4,9 +4,9 @@
             <div class="row align-items-center">
                 <div class="col-lg-12 col-md-12">
                     <div class="features-item-free-quotes with-border bg-grey">
-                        <h5>Did you know?</h5>
-                        <h3>Automated Quote Generation tool helps businesses achieve faster sales cycles and higher win rates by providing accurate pricing and quotes in a timely manner. <br> Get started with a demo session and free onboarding today.</h3>
-                        <router-link to="/" class="default-btn">Sign up now!</router-link>
+                        <h3>Automating quote generation boosts creativity and streamlines workflow, Fact-based quotes
+                            maintain accuracy while keeping a unique voice.</h3>
+                        <router-link to="/" class="default-btn">Get your free quotes now!</router-link>
                     </div>
                 </div>
             </div>
