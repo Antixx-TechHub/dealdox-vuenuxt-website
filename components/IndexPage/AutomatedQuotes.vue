315bdcaa0ca09f8d1886a9c5f09d9ed8d4f7dd21
AntixxTechHub <info@antixxtechhub.com>
2023-06-14 14:42:05 +0530

    Home page added new componetnt

---
+++ components/IndexPage/AutomatedQuotes.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="free-trial-area ptb-100 bg-FFFFFF">
+        <div class="container">
+            <div class="section-title title-with-bg-text">
+                <h2>Enhance Your Quote Generation Process with<br>the best CPQ Solution Now</h2>
+                <p>Let’s get started!</p>
+            </div>
+            <div class="free-trial-content">
+                <div class="row">
+                    <div class="section-title title-with-bg-text">
+                <h2>Enjoy DealDox at zero cost! Including:</h2>
+            </div>
+                    <div class="col-lg-6 col-md-6">
+                        <div class="software-integrations-list">
+                            <img src="../../assets/images/HomePage/easy-integration.png" alt="border">
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6">
+                        <div class="features-content">
+                            <ul class="features-list">
+                                <h2>Generate <span class="sub-title">Automated Quotes </span> in a few Seconds! Save Time and Close More Deals</h2>
+                                <li>
+                                    <div class="icon">
+                                        <i class="ri-check-line"></i>
+                                    </div>
+                                    <p class="with-top-border">Best Solution for quick and accurate quote generation in less time</p>
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <i class="ri-check-line"></i>
+                                    </div>
+                                    <p class="with-top-border">Eliminate manual quote creation</p>
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <i class="ri-check-line"></i>
+                                    </div>
+                                    <p>Close deals faster and win more business</p>
+                                </li>
+                            </ul>
+
+                            <div class="btn-box">
+                                <router-link to="/" class="default-btn">Know more</router-link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="lines">
+                    <div class="line"></div>
+                    <div class="line"></div>
+                    <div class="line"></div>
+                    <div class="line"></div>
+                    <div class="line"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AutomatedQuotes'
+}
+</script>
