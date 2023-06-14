96bdf66304a37408ba4aa0dccf79b02b853d2dff
AntixxTechHub <info@antixxtechhub.com>
2023-06-14 10:01:23 +0530

    Base url change

--- nuxt.config.js
+++ nuxt.config.js
@@ -72,7 +72,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://cms.antixxtechhub.in/api',
+    baseURL: 'https://dealdox.pbwebvision.com/api',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
