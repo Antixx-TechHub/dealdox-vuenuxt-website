ae62e9bfd95aa9a69c8716d901bcb6bcb3106d44
Srohanrs <prashant.bhosale@antixxtechhub.com>
2023-06-17 18:13:02 +0530

    mobile responsive done

--- components/CPQIntegration/Stats.vue
+++ components/CPQIntegration/Stats.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="key-features-area bg-#FAFAFA pt-100 pb-75">
         <div class="container">
-            <div class="section-title title-with-bg-text">
+            <div class="section-titles title-with-bg-text">
                 <h2>Bridge the gap between CRM and ERP.</h2>
                 <p>DealDox streamlines service contracting for IT providers, increasing profit margins while reducing effort
                     and risk. Our AI-enabled quoting solution ensures fast and accurate statements of work with necessary
