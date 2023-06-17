647f1ece5137d18565e46a4cc51f6ce2ae2f0295
AntixxTechHub <info@antixxtechhub.com>
2023-06-17 12:51:33 +0530

    base url change

--- components/Common/Partner.vue
+++ components/Common/Partner.vue
@@ -34,7 +34,7 @@ export default {
         partners: null,
     }),
     created: async function () {
-        const response = await axios.get('https://dealdoxstrapi.pbwebvision.com/api/partner?populate=partnerSlides.image')
+        const response = await axios.get('http://localhost:1337/api/partner?populate=partnerSlides.image')
         const { data: { attributes } } = response.data
         this.partners = attributes
     },
