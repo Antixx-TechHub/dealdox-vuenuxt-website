e0ffb4fdc12a83e5ee97a71447352971c9cf5ff5
Srohanrs <prashant.bhosale@antixxtechhub.com>
2023-06-21 16:49:56 +0530

    changes done

---
+++ components/LocalizedDealDocuments/MainBannerLoacalized.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="new-app-main-banner-wrap-area bg-white">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-md-12">
+                    <div class="new-app-main-banner-wrap-content">
+                        <h1><span class="sub-title">Localized Deal </span> Documents</h1>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12">
+                    <div class="new-app-main-banner-wrap-image" data-aos="fade-left" data-aos-duration="2000">
+                        <img src="../../assets/images/cpq/cpq-banner.png" alt="image">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="new-app-main-banner-wrap-shape">
+            <img src="../../assets/images/more-home/banner/shape-5.png" alt="image">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainBannercpq'
+}
+</script>
---
+++ pages/localizeddealdocuments.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <Navbar class="navbar-style-two" />
+        <MainBannerLoacalized/>
+        <HowLocalizedDeal />
+        <KeyBenefitsof />
+        <FreeTrial />
+        <DealDoxFooter />
+    </div>
+</template>
+
+<script>
+import Navbar from '../layouts/Navbar'
+import MainBannerLoacalized from '../components/LocalizedDealDocuments/MainBannerLoacalized'
+import HowLocalizedDeal from '../components/LocalizedDealDocuments/HowLocalizedDeal'
+import KeyBenefitsof from '../components/LocalizedDealDocuments/KeyBenefitsof'
+import FreeTrial from '../components/LocalizedDealDocuments/FreeTrial'
+import DealDoxFooter from '../layouts/DealDoxFooter'
+
+export default {
+    name: 'AboutPageOne',
+    components: {
+        Navbar,
+        MainBannerLoacalized,
+        HowLocalizedDeal,
+        KeyBenefitsof,
+        FreeTrial,
+        DealDoxFooter,
+    },
+    head: {
+        title: 'CPQ (Configure, Price, Quote)|DealDox',
+        htmlAttrs: {
+            lang: 'en-us'
+        },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: 'Streamline quoting processes with DealDox CPQ. Configure, price, and quote with precision, empowering sales teams for success.' },
+            { hid: 'keywords', name: 'keywords', content: 'CPQ(Configure, Price, Quote), best cpq software, price quote software, configure price quote (cpq) software, automate presales process, CPQ guided selling, guidded selling cpq, best quote automation software, Automated Quoting Solutions, AI-enabled CPQ for Services, Quote-to-cash process, Advanced guided selling, cpq guided selling setup, automate the process of configuring products sales tool to generate quick quotes accurately, automated quoting software, best tool for sales engagement, CPQ Quoting Process' }
+        ],
+        link: [{ hid: 'canonical', rel: 'canonical', href: 'https://www.dealdox.io/cpq' }
+        ],
+    },
+}
+</script>
