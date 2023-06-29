1a1d313bb11624204bcda061f4c7f674ca457efe
Srohanrs <prashant.bhosale@antixxtechhub.com>
2023-06-29 16:35:58 +0530

    requestDemo Done

---
+++ components/RequestDemo/TransformDealsWith.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="features-area pb-85 bg-FAFAFA">
+        <div class="container">
+            <div class="software-integrations-inner bg-gradient-color">
+                <div class="row align-items-center">
+                    <div class="col-lg-6 col-md-12">
+                        <div class="features-content">
+                            <ul class="features-list">
+                                <h2>Transform Deals with <span class="sub-title">DealDox</span>in Action!</h2>
+                                <p>A centralized platform for automated quote generation, Accurate Pricing, Product Bundling and Upselling, Margin Optimization, Cost Visibility, Efficient Pricing Strategy, Customize Deal Documents in your desired language, and more! </p>
+                                <li>
+                                    <div class="icon">
+                                        <i class="ri-check-line"></i>
+                                    </div>
+                                    <p class="with-top-border">Easy to Use</p>
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <i class="ri-check-line"></i>
+                                    </div>
+                                    <p class="with-top-border">Totally Unified</p>
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <i class="ri-check-line"></i>
+                                    </div>
+                                    <p>Close Deals Faster</p>
+                                </li>
+                            </ul>
+
+                            <div class="btn-box">
+                                <router-link to="/lightning-fast-quote-approvals" class="default-btn">Know more</router-link>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 col-md-12">
+                        <div class="software-integrations-list">
+                            <img src="../../assets/images/RequestDemo/Frame 1000007144.png" alt="border">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'EasyIntegration'
+}
+</script>
---
+++ pages/requestdemo.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <Navbar class="navbar-style-two" />
+        <TransformDealsWith />
+        <Partner />
+        <DealDoxFooter />
+        <!-- <PageHeader /> -->
+        <!-- <Blog /> -->
+        <!-- <Faq /> -->
+    </div>
+</template>
+  
+<script>
+
+import Navbar from '../layouts/Navbar';
+import PageHeader from '../components/ContactUs/PageHeader'
+import TransformDealsWith from '../components/RequestDemo/TransformDealsWith'
+import Partner from '../components/Common/Partner'
+import Blog from '../components/Common/Blog'
+import Faq from '../components/Common/Faq';
+import DealDoxFooter from '../layouts/DealDoxFooter';
+
+
+export default {
+    components: {
+        Navbar,
+        PageHeader,
+        TransformDealsWith,
+        Partner,
+        Blog,
+        Faq,
+        DealDoxFooter,
+    },
+    head: {
+        title: '',
+        htmlAttrs: {
+            lang: 'en-us'
+        },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' },
+            { hid: 'keywords', name: 'keywords', content: '' }
+        ],
+        link: [{ hid: 'canonical', rel: 'canonical', href: '#' }
+        ],
+    },
+}
+</script>
