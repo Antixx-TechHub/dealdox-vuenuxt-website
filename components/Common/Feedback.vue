a0b2e30ad167f19c85553bd012871eb9ed5ca984
AntixxTechHub <info@antixxtechhub.com>
2023-07-03 20:24:22 +0530

    feedback

--- components/Common/Feedback.vue
+++ components/Common/Feedback.vue
@@ -8,13 +8,14 @@
                             <div class="tabs__list-tab" v-for="customerreview in customerreviews"
                                 v-bind:class="{ 'active': activeTab == customerreview.id }" :key="customerreview.id">
                                 <div class="features-content">
-                                    <h1>What does users say about<span class="sub-title">Dealdox </span></h1>
+                                    <h1>What does users say about <span class="sub-title">Dealdox</span></h1>
                                     <br>
                                     <br>
                                     <carousel :autoplay="8000" :settings='settings' :loop="true"
                                         :perPageCustom="[[0, 1], [576, 1], [768, 1], [1200, 1], [1400, 1]]"
                                         :paginationEnabled="false" :breakpoints='breakpoints'>
-                                        <slide v-for="customerreview in customerreviews.singleFeedbacks">
+                                        <slide v-for="customerreview in customerreviews.singleFeedbacks"
+                                            :key="customerreview.id">
                                             <h6>{{ customerreview.quite }}</h6>
                                             <p>{{ customerreview.shortDec }}</p>
                                             <h6>
@@ -24,7 +25,6 @@
                                             <div class="tabs">
                                                 <ul class="tabs__header">
                                                     <li class="tabs__header-item">
-                                                        <!-- <img :src="customerreview.media" alt="user"> -->
                                                         <img :src="customerreview.media.data.attributes.url" alt="image">
                                                     </li>
                                                 </ul>
