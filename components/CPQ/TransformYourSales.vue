5ae0f2503aea12a0e577851a552b477e3d2f3acb
Srohanrs <prashant.bhosale@antixxtechhub.com>
2023-07-05 18:39:41 +0530

    changes updated

--- components/CPQ/TransformYourSales.vue
+++ components/CPQ/TransformYourSales.vue
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="section-title-pages-full">
                 <div class="new-app-main-banner-wrap-content-full">
-                    <h2>Transform Your Sales Process with Our <span class="sub-title">Quoting and Order Management CPQ</span> Solutions</h2>
+                    <h1>Transform Your Sales Process with Our <span class="sub-title">Quoting and Order Management CPQ</span> Solutions</h1>
                 </div>
                 <div class="stats-map text-center">
                     <img src="../../assets/images/banner/banner-home.png" alt="cpq">
