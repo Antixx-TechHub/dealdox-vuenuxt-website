5af9ede5c01a6b981ce214fbb929f91c720ddc75
AntixxTechHub <info@antixxtechhub.com>
2023-07-26 16:43:03 +0530

    New changes

--- components/CPQ/MainBannerCPQ.vue
+++ components/CPQ/MainBannerCPQ.vue
@@ -1,5 +1,30 @@
 <template>
-    <div class="new-app-main-banner-wrap-area bg-white">
+<div class="new-app-main-banner-wrap-area bg-white">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-md-12">
+                    <div class="new-app-main-banner-wrap-content">
+                        <h1>Best <span class="sub-title">CPQ</span> - Deliver timely Sales Quotes Faster and More Accurately</h1>
+                        <p>Empower your sales team with DealDox for faster and more accurate quotes for timely deal delivery.</p>
+                    </div>
+                </div> 
+                <div class="col-lg-6 col-md-12">
+                    <div class="new-app-main-banner-wrap-image" data-aos="fade-left" data-aos-duration="2000">
+                        <img src="../../assets/images/cpq-integration/cpq-integration-banner.png" alt="image">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="new-app-main-banner-wrap-shape">
+            <img src="../../assets/images/more-home/banner/shape-5.png" alt="image">
+        </div>
+    </div>
+
+
+
+
+    <!-- <div class="new-app-main-banner-wrap-area bg-white">
         <div class="container-fluid">
             <div class="row align-items-center">
                 <div class="col-lg-6 col-md-12">
@@ -25,7 +50,7 @@
         <div class="new-app-main-banner-wrap-shape">
             <img src="../../assets/images/more-home/banner/shape-5.png" alt="image">
         </div>
-    </div>
+    </div> -->
 </template>
 
 <script>
