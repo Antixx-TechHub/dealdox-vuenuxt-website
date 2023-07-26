9479b0031fb135dc620e5ff5c43c69b69c1a153e
AntixxTechHub <info@antixxtechhub.com>
2023-07-26 15:57:51 +0530

    6th update

--- components/Subscriptions/OnbaordingAssistance.vue
+++ components/Subscriptions/OnbaordingAssistance.vue
@@ -44,7 +44,7 @@
                                     </li>
                                 </ul>
                                 <div class="btn-box">
-                                    <router-link to="/" class="default-btn-home">Start My Free Trial</router-link>
+                                    <router-link to="/sign-up-free-trial" class="default-btn-home">Start My Free Trial</router-link>
                                 </div>
                             </div>
                         </div>
