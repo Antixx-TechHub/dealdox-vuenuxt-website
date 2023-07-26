6c1f207e13eeac15b37eaee7d9e5c4856a57f4dc
Srohanrs <prashant.bhosale@antixxtechhub.com>
2023-07-26 15:39:03 +0530

    SugarCRM DOne

--- components/Sugar-CRM/KeyBenefitsof.vue
+++ components/Sugar-CRM/KeyBenefitsof.vue
@@ -2,7 +2,7 @@
     <div class="key-features-area bg-FAFAFA ptb-100">
         <div class="container">
             <div class="section-title-pages-full">
-                <div class="new-app-main-banner-wrap-content-full tb-30">
+                <div class="new-app-main-banner-wrap-content-full tb-20">
                     <h3>Expand Your <span class="sub-title-effortless">CRM Horizons</span> with DealDox</h3>
                 </div>
             </div>
@@ -10,7 +10,7 @@
                 <div class="col-xl-3 col-lg-6 col-sm-6 col-md-6">
                     <div class="single-features-item">
                         <div class="icon">
-                            <img src="../../assets/images/cpq-integration/cpq-integration-banner-icon1.png" alt="image">
+                            <img src="../../assets/images/Hubspot/Swift Quote.png" alt="image">
                         </div>
                         <h3>Swift Quotes</h3>
                         <p>Generate quotations and any deal documents in a few seconds.</p>
@@ -18,10 +18,9 @@
                     </div>
                 </div>
                 <div class="col-xl-3 col-lg-6 col-sm-6 col-md-6">
-                    <!-- <div class="single-features-box bg-color-two"> -->
-                        <div class="single-features-item">
+                    <div class="single-features-item">
                         <div class="icon">
-                            <img src="../../assets/images/cpq-integration/cpq-integration-banner-icon2.png" alt="image">
+                            <img src="../../assets/images/Hubspot/Quick Approvals.png" alt="image">
                         </div>
                         <h3>Quick Approvals</h3>
                         <p>Complex approvals are made easy with DealDox - it simplifies the sales process.
@@ -32,9 +31,9 @@
                 <div class="col-xl-3 col-lg-6 col-sm-6 col-md-6">
                     <div class="single-features-item">
                         <div class="icon">
-                            <img src="../../assets/images/cpq-integration/predictioncpq.png" alt="image">
-                            </div>
-                        <h3>Configure Price Quote (CPQ)</h3>
+                            <img src="../../assets/images/Hubspot/Configure Price Quote.png" alt="image">
+                            </div>                        
+                            <h3>Configure Price Quote (CPQ)</h3>
                         <p>Easy configurations and error-free quotes in minutes.</p>
                         <router-link to="/cpq" class="default-btn">Know more</router-link>
                     </div>
@@ -42,7 +41,7 @@
                 <div class="col-xl-3 col-lg-6 col-sm-6 col-md-6">
                     <div class="single-features-item">
                         <div class="icon">
-                            <img src="../../assets/images/cpq-integration/Automated-Oppertunity.png" alt="image">
+                            <img src="../../assets/images/Hubspot/Subscription.png" alt="image">
                         </div>
                         <h3>Subscription</h3>
                         <p>Upselling, Expand and Automated renewals. Pay only for what you use.</p>
