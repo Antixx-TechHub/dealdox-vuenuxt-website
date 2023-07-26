6fd0049db153938090d260bf0bc692ac863b42ef
AntixxTechHub <info@antixxtechhub.com>
2023-07-26 22:25:30 +0530

    final work

--- components/LocalizedDealDocuments/MainBannerLoacalized.vue
+++ components/LocalizedDealDocuments/MainBannerLoacalized.vue
@@ -4,7 +4,7 @@
             <div class="row align-items-center">
                 <div class="col-lg-6 col-md-12">
                     <div class="new-app-main-banner-wrap-content">
-                        <h1>Introducing Localized Deal Documents: Unlocking <span class="sub-title">Multilingual</span><span
+                        <h1>Introducing Localized Deal Documents: Unlocking <span style="color: #715CF3">Multilingual</span><span
                                 class="sub-title">Communication</span> for Global Business</h1>
                                 <div class="tb-30">
                             <router-link to="/requestdemo" required class="default-btn"><i class='bx bx-paper-plane'></i>
