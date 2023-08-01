e22e34aa261d4fb2e87951e0e7c44a4838226179
AntixxTechHub <info@antixxtechhub.com>
2023-08-01 10:39:51 +0530

    done

--- components/RequestDemo/demo.vue
+++ components/RequestDemo/demo.vue
@@ -68,18 +68,5 @@ export default {
             iframeUrl: 'https://spmglobaltech.my.salesforce-sites.com/requestdemo', // Initial URL for the iframe
         };
     },
-    methods: {
-    refreshPage() {
-      // Delay the page refresh by 2 seconds (2000 milliseconds)
-      setTimeout(() => {
-        // Redirect the user to the current page URL, effectively refreshing the page
-        this.$nuxt.$router.replace(this.$nuxt.$route.fullPath);
-      }, 500);
-    },
-  },
-  mounted() {
-    // Call the refreshPage method when the component is mounted
-    this.refreshPage();
-  },
 }
 </script>
