4dd45c3e12f60296601701c05f5315819dd7894f
Srohanrs <prashant.bhosale@antixxtechhub.com>
2023-08-01 19:35:27 +0530

    links added in form bottom

--- components/SignUp/SignUpHeader.vue
+++ components/SignUp/SignUpHeader.vue
@@ -118,7 +118,8 @@
                                                 <div class="form-group">
                                                     <p>Job Title</p>
                                                     <input type="text" maxlength="40" name="Title" required
-                                                        class="form-control" id="Title" placeholder="Eg: Associate Consultant">
+                                                        class="form-control" id="Title"
+                                                        placeholder="Eg: Associate Consultant">
                                                 </div>
                                             </div>
 
@@ -367,6 +368,21 @@
                                                 </div>
                                             </div>
 
+                                            <div class="col-lg-12 col-md-12 col-sm-12">
+                                                <div class="form-group">
+                                                    <input class="form-check-input" type="checkbox" value=""
+                                                        id="flexCheckDefault">
+                                                    <label class="form-check-label" for="flexCheckDefault">
+                                                        I agree to the <router-link to="/terms-of-use"> Terms of
+                                                            Use</router-link>
+                                                    </label>
+                                                </div>
+                                            </div>
+
+                                            <p> By registering, you confirm that you agree to the storing and processing of
+                                                your personal data by DealDox as described in the<router-link
+                                                    to="/privacy-policy">
+                                                    Privacy </router-link> Statement. </p>
 
                                             <tr style="display: none;">
                                                 <td><label for="Sign_Up__c">Sign Up</label></td>
