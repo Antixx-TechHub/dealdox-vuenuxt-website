48110b8a696825742465cf30236b901768861ab7
AntixxTechHub <info@antixxtechhub.com>
2023-08-01 09:08:32 +0530

    Thank you page reset

--- pages/thank-you.vue
+++ pages/thank-you.vue
@@ -19,21 +19,16 @@ export default {
         ThankYouPage,
         DealDoxFooter,
     },
-    methods: {
-        closePageAfterDelay() {
-            setTimeout(() => {
-                // Close the current page or redirect to another page
-                // You can use window.close() to close the current tab/window.
-                // Note: window.close() may not work in some browsers if the window was not opened by a script.
-                // Alternatively, you can use Vue Router's `push` method to navigate to another page.
-                // For example:
-                // this.$router.push('/another-page')
-            }, 5000); // 5000 milliseconds = 5 seconds (adjust the delay as needed)
-        },
-    },
     mounted() {
-        // Call the method when the component is mounted to start the countdown.
-        this.closePageAfterDelay();
+    this.refreshPageAfterDelay();
+  },
+  methods: {
+    refreshPageAfterDelay() {
+      setTimeout(() => {
+        // Reload the current page after 5 seconds (5000 milliseconds)
+        window.location.reload();
+      }, 5000);
     },
+  },
 }
 </script>
