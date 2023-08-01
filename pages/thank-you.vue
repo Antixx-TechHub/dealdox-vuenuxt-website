3570301263f3bd7ee11101fb1beff66ec25975bb
AntixxTechHub <info@antixxtechhub.com>
2023-08-01 14:30:39 +0530

    Final Form submit

--- pages/thank-you.vue
+++ pages/thank-you.vue
@@ -1,33 +1,26 @@
 <template>
   <div>
-    <!-- <Navbar class="navbar-style-two" /> -->
+    <Navbar class="navbar-style-two" />
     <ThankYouPage />
-    <!-- <DealDoxFooter /> -->
+    <DealDoxFooter />
   </div>
 </template>
   
 <script>
 
-// import Navbar from '../layouts/Navbar';
+import Navbar from '../layouts/Navbar';
 import ThankYouPage from '../components/ThankYou/ThankYouPage'
-// import DealDoxFooter from '../layouts/DealDoxFooter';
+import DealDoxFooter from '../layouts/DealDoxFooter';
 
 
 export default {
   components: {
-    // Navbar,
+    Navbar,
     ThankYouPage,
-    // DealDoxFooter,
+    DealDoxFooter,
   },
-  // mounted() {
-  //   // Redirect to the external URL after a short delay (e.g., 3 seconds)
-  //   setTimeout(() => {
-  //     window.location.href = 'https://dealdox.pbwebvision.com/request-a-demo';
-  //   }, 5000); // 3 seconds delay before redirecting
-  // },
-
-  // created() {
-  //   setTimeout(() => this.$router.push({ path: '/request-a-demo' }), 5000)
-  // }
+  created() {
+    setTimeout(() => this.$router.push({ path: '/' }), 5000)
+  }
 }
 </script>
