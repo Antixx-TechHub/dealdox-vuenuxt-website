b8e56086a0724663c76e7e2bafbc15ef45560022
Srohanrs <prashant.bhosale@antixxtechhub.com>
2023-08-02 19:14:37 +0530

    form validation addded

--- components/SignUp/SignUpHeader.vue
+++ components/SignUp/SignUpHeader.vue
@@ -415,7 +415,84 @@
 </template>
 
 <script>
+new Vue({
+    el: '#contact-form',
+    data: {
+      formData: {
+        first_name: '',
+        last_name: '',
+        phone: '',
+        email: '',
+        company: '',
+        country: '',
+        message: '',
+        agree_terms: true
+      },
+      errors: {}
+    },
+});
+
+import Vue from 'vue'
+
 export default {
+
+    methods: {
+      validateForm() {
+        this.errors = {};
+
+        if (!this.formData.first_name) {
+          this.errors.first_name = 'First Name is required.';
+        }
+
+        if (!this.formData.last_name) {
+          this.errors.last_name = 'Last Name is required.';
+        }
+
+        if (!this.formData.phone) {
+          this.errors.phone = 'Phone is required.';
+        }
+
+        if (!this.formData.email) {
+          this.errors.email = 'Email is required.';
+        } else if (!this.isValidEmail(this.formData.email)) {
+          this.errors.email = 'Please enter a valid email address.';
+        }
+
+        if (!this.formData.company) {
+          this.errors.company = 'Company is required.';
+        }
+
+        if (!this.formData.country) {
+          this.errors.country = 'Country is required.';
+        }
+
+        if (!this.formData.message) {
+          this.errors.message = 'Message is required.';
+        }
+
+        if (!this.formData.agree_terms) {
+          this.errors.agree_terms = 'You must agree to the Terms of Use.';
+        }
+
+        return Object.keys(this.errors).length === 0;
+      },
+      isValidEmail(email) {
+        // You can implement your own email validation logic here.
+        // For a simple example, let's check if the email contains '@'.
+        return email.includes('@');
+      },
+      onSubmit() {
+        if (this.validateForm()) {
+          // Submit the form
+          // For example, you can use axios or fetch to submit the form data to the server.
+          // You can also redirect to the 'thank-you' page after successful submission.
+        //   alert('Form submitted successfully!');
+          // this.$refs.contactForm.submit(); // Uncomment this line if you want to submit the form using HTML form submission.
+        }
+      }
+    },
+
+
     name: 'AppDownload',
 }
 </script>
